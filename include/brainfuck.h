8f53ef6c4210e6e48f8d42aee2230db3d2621253
jsob0 <jakub.sobczak.main@gmail.com>
2022-12-17 20:26:54 +0100

    clearer code (rewritten)

---
+++ include/brainfuck.h
@@ -0,0 +1,94 @@
+#ifndef BRAINFUCK_H
+#define BRAINFUCK_H
+
+// The current version of the program.
+#define BRAINFUCK_VERSION "1.1"
+
+// The maximum size of the data on which the user can operate.
+#define BRAINFUCK_DATA_SIZE 30000
+
+// The maximum size of a single byte in the data.
+#define BRAINFUCK_BYTE_SIZE 255
+
+/* Brainfuck opeartors */
+
+#define BRAINFUCK_OP_NEXT '>'
+#define BRAINFUCK_OP_PREV '<'
+#define BRAINFUCK_OP_ADD '+'
+#define BRAINFUCK_OP_SUB '-'
+#define BRAINFUCK_OP_OUTPUT '.'
+#define BRAINFUCK_OP_INPUT ','
+#define BRAINFUCK_OP_LOOP_START '['
+#define BRAINFUCK_OP_LOOP_END ']'
+
+// The path from which the file will be readed.
+char brainfuck_path_input[260];
+
+// The path where the output message will be saved.
+char brainfuck_path_output[260];
+
+/**
+ * Interpret and execute brainfuck code from the passed source.
+ *
+ * @param source The string storing brainfuck code to interpretation and
+ *      execution.
+ *
+ * @returns If the function succeeds, the return value is nonzero.
+ */
+int brainfuck_interpret(char *source);
+
+/**
+ * Read brainfuck code from the passed path.
+ *
+ * @param path The path of the file from which the file will be read.
+ *
+ * @returns Dynamically allocated string with brainfuck code.
+ */
+char *brainfuck_read_file(const char *path);
+
+/**
+ * Print in the console information about the error that occurred in
+ * the brainfuck code.
+ *
+ * @param ch The number of the character in which the error occurred.
+ * @param fmt A printf() style message format string.
+ * @param ... Additional parameters matching % tokens in the fmt string,
+ *      if any.
+ */
+void brainfuck_err_code(int ch, const char *fmt, ...);
+
+/**
+ * Print in the console information about the error that occurred during
+ * the initialization of the interpreter.
+ *
+ * @param ch The number of the character in which the error occurred.
+ * @param fmt A printf() style message format string.
+ * @param ... Additional parameters matching % tokens in the fmt string,
+ *      if any.
+ */
+void brainfuck_err_init(const char *fmt, ...);
+
+/* Messages for user */
+
+const char *BF_message_help = "\
+Usage: %s [OPTIONS] [FILE/-e CODE]\n\
+\n\
+Interpret brainfuck code by passing the file or the code itself.\n\
+\n\
+Available options:\n\
+  -h, --help          Show this help message.\n\
+  -v, --version       Show the program version.\n\
+  -o, --output FILE   Write output to FILE.\n\
+  -e, --execute CODE  Execute the code specified by CODE.\n\
+\n\
+For more information or to report bugs, add me on discord sopa#7147\n\
+";
+
+const char *BF_message_version = "\
+Brainfuck %s version.\n\
+\n\
+Copyright (c) 2022 Jakub Sobczak.\n\
+Distributed under the MIT License.\n\
+";
+
+#endif /* BRAINFUCK_H */
--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,175 +1,183 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
+#include <stdarg.h>
 #include <time.h>
 
-#define VERSION "1.0.0"
-
-// The maximum size of the data on which the user can operate
-#define MEM_MAX_SIZE 30000
-
-// The maximum size of a single byte in the data
-#define BYTE_MAX_SIZE 255
-
-// Variable for error handling, so that we do not have to specify each time
-// the function path to the compiling file.
-char bf_compile_filepath[128] = { 0 };
-
-/*
- * Compile the brainfuck code.
- *
- * \param source The string with the code to compile.
- *
- * \returns If the compilation succeeds, the return value is nonzero.
- */
-int compile(char *source);
-
-/*
- * Read the brainfuck code from the specified file.
- * 
- * \param filepath The path to the file to read.
- * 
- * \returns Dynamically allocated string with the code or NULL if the
- *			read fails
- */
-char *read_code(const char *filepath);
-
-/*
- * Error handling, write in the console information about the error and in
- * which character it was.
- *
- * \param ch The number of the character in which the error occurred.
- * \param fmt A printf() style message format string.
- * \param ... Additional parameters matching % tokens in the fmt string,
- *            if any.
- * 
- * Example message:
- * 
- * File "C:\users\user\hello_world.bf", char 43
- * Error: Byte overflow (256)
- */
-void errprintf(int ch, const char *fmt, ...);
+#include <brainfuck.h>
 
 int main(int argc, char **argv) {
-    if (2 != argc ||
-        strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-        fprintf(stderr, "usage: %s [-vh] / [file...]\n", argv[0]);
-        printf("\t-v --version\tshow interpreter version.\n");
-        printf("\t-h --help\tshow help menu.\n");
-        return 0;
+    // The string stores the source code of brainfuck.
+    char *source = NULL;
+
+    // We start the iteration with a one, because we skip the first argument,
+    // which is the name of this program.
+    int i = 1;
+    do {
+        if (1 == argc || !strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+            printf(BF_message_help, argv[0]);
+            return 0;
+        }
+        else if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--version")) {
+            printf(BF_message_version, BRAINFUCK_VERSION);
+            return 0;
+        }
+        else if (!strcmp(argv[i], "-o") || !strcmp(argv[i], "--output")) {
+            // We need to check if the next argument, which is the output
+            // path exists, because the user may not have typed it.
+            if (argc < i + 1) {
+                strcpy(brainfuck_path_output, "out.txt");
+            }
+            else {
+                strcpy(brainfuck_path_output, argv[i + 1]);
+            }
+        }
+        else if (!strcmp(argv[i], "-e") || !strcmp(argv[i], "--execute")) {
+            // We need to check if the next argument, which is the source
+            // code exists, because the user may not have typed it.
+            if (argc < i + 1) {
+                return 1;
+            }
+
+            // The user can use the execute argument many times, so we need
+            // to release the previously allocated memory to avoid
+            // memory leaks.
+            if (source) {
+                free(source);
+            }
+
+            // Copy the source code from the argument to the source variable.
+            size_t source_len = strlen(argv[i + 1]);
+            source = (char *)malloc(source_len + 1);
+            if (NULL == source) {
+                brainfuck_err_init("Failed to allocate memory space.\n");
+                return 1;
+            }
+
+            strcpy(source, argv[i + 1]);
+            strcpy(brainfuck_path_input, "console input");
+		}
+		++i;
+    } while (i < argc);
+
+	if (NULL == source) {
+		// Set the path input if the user has not chose the execute argument.
+		strcpy(brainfuck_path_input, argv[argc - 1]);
+        source = brainfuck_read_file(brainfuck_path_input);
+        if (NULL == source) return 1;
     }
-    
-    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
-        printf("brainfuck %s version.\n", VERSION);
-        printf("Copyright (c) 2022 Jakub Sobczak.\n");
-        printf("Distributed under the MIT License.\n");
-        return 0;
-    }
 
-    strcpy(bf_compile_filepath, argv[1]);
+	clock_t t;
+	t = clock();	
 
-    char *source = read_code(bf_compile_filepath);
-    if (NULL == source) {
-        // error handling in the function
-        return 1;
-    } /* If the read fails */
-
-    clock_t t;
-    t = clock();
-
-    if (0 != compile(source)) {
-        t = clock() - t;
-        double time_taken = ((double)t) / CLOCKS_PER_SEC;
-        printf("\nThe program took %f seconds to execute.\n", time_taken);
-    } /* If the compilation succeeds */
+    if (0 != brainfuck_interpret(source)) {
+		t = clock() - t;
+		double time_taken = (double)t / CLOCKS_PER_SEC;
+		printf("\nThe program took %f seconds to execute.\n", time_taken);
+	}
 
     free(source);
     return 0;
 }
 
-int compile(char *source) {
+int brainfuck_interpret(char *source) {
     // Data for brainfuck code, filled with zeros and a pointer which points
     // on the current data.
-    unsigned char *data = (unsigned char *)malloc(MEM_MAX_SIZE);
+    unsigned char *data = (unsigned char *)malloc(BRAINFUCK_DATA_SIZE);
     if (NULL == data) {
-        fprintf(stderr, "Failed to allocate memory for a data.\n");
+        brainfuck_err_init("Failed to allocate memory space.\n");
         return 1;
-    } /* if failed to allocate memory for a data */
-    memset(data, 0, MEM_MAX_SIZE);
+    }
+    memset(data, 0, BRAINFUCK_DATA_SIZE);
 
     int ptr = 0, isError = 0, ip = 0;
     int source_len = strlen(source);
 
+    // Open the output file if the user has set it.
+	FILE *file_output = NULL;
+	if (0 != *brainfuck_path_output) {
+		file_output = fopen(brainfuck_path_output, "w");
+		if (NULL == file_output) {
+			brainfuck_err_init("Unable to open the output file `%s`\n",
+					brainfuck_path_output);
+			return 1;
+		}
+	}
+
     while (ip < source_len && !isError) {
         switch (source[ip]) {
         // Increment the data pointer.
-        case '>':
-            if (MEM_MAX_SIZE == ++ptr) {
-                errprintf(ip, "Pointer index out of range (%d).\n",
-                    MEM_MAX_SIZE + 1);
+        case BRAINFUCK_OP_NEXT:
+            if (BRAINFUCK_DATA_SIZE == ++ptr) {
+                brainfuck_err_code(ip, "Pointer index out of range (%d).\n",
+                    BRAINFUCK_DATA_SIZE + 1);
                 ++isError;
             }
             break;
         // Decrement the data pointer.
-        case '<':
+        case BRAINFUCK_OP_PREV:
             if (-1 == --ptr) {
-                errprintf(ip, "Pointer index out of range (-1).\n");
+                brainfuck_err_code(ip, "Pointer index out of range (-1).\n");
                 ++isError;
             }
             break;
         // Increment the byte at the data pointer.
-        case '+':
-            if (BYTE_MAX_SIZE == ++data[ptr]) {
-                errprintf(ip, "Byte overflow (%d).\n", BYTE_MAX_SIZE + 1);
+        case BRAINFUCK_OP_ADD:
+	    // Compare the byte to zero, because when byte overflows value
+		// returns to the beginning of the.
+            if (0 == ++data[ptr]) {
+                brainfuck_err_code(ip, "Byte overflow (%d).\n",
+                    BRAINFUCK_BYTE_SIZE + 1);
                 ++isError;
             }
             break;
         // Decrement the byte at the data pointer.
-        case '-':
-            if (-1 == --data[ptr]) {
-                errprintf(ip, "Byte overflow (-1).\n");
+        case BRAINFUCK_OP_SUB:
+	    // Compare the byte to its max, because when byte overflows value
+		// returns to the end of the.
+            if (BRAINFUCK_BYTE_SIZE == --data[ptr]) {
+                brainfuck_err_code(ip, "Byte overflow (-1).\n");
                 ++isError;
             }
             break;
         // Output the byte at the data pointer.
-        case '.':
-            putchar(data[ptr]);
+        case BRAINFUCK_OP_OUTPUT:
+			fputc(data[ptr], (file_output) ? file_output : stdout);
             break;
         // Input a byte and store it at the data pointer.
-        case ',':
+        case BRAINFUCK_OP_INPUT:
             data[ptr] = getchar();
             break;
         // If the byte at the data pointer is 0, jump to the corresponding ']'
-        case '[': {
+		case BRAINFUCK_OP_LOOP_START: {
             if (0 == data[ptr]) {
-                int depth = 1, ipError = ip;;
+                int depth = 1, ipError = ip;
                 while (depth) {
-                    if (++ip == source_len) {
-                        errprintf(ipError, "End of loop not found.\n");
+                    if (source_len == ++ip) {
+                        brainfuck_err_code(ipError, "End of loop not found.\n");
                         ++isError;
                         break;
                     }
 
-                    if (source[ip] == '[') ++depth;
-                    else if (source[ip] == ']') --depth;
+                    if ('[' == source[ip]) ++depth;
+                    else if (']' == source[ip]) --depth;
                 }
             }
         } break;
         // If the byte at the data pointer is not 0, jump to the
         // corresponding '['
-        case ']': {
+        case BRAINFUCK_OP_LOOP_END: {
             if (0 != data[ptr]) {
                 int depth = 1, ipError = ip;
                 while (depth) {
-                    if (--ip == -1) {
-                        errprintf(ipError, "Start of loop not found.\n");
+                    if (-1 == --ip) {
+                        brainfuck_err_code(ipError, "Start of loop not found.\n");
                         ++isError;
                         break;
                     }
 
-                    if (source[ip] == '[') --depth;
-                    else if (source[ip] == ']') ++depth;
+                    if ('[' == source[ip]) --depth;
+                    else if (']' == source[ip]) ++depth;
                 }
             }
         } break;
@@ -181,43 +189,61 @@ int compile(char *source) {
         ++ip;
     }
 
+    if (file_output)
+		fclose(file_output);
     free(data);
     return !isError;
 }
 
-char *read_code(const char *filepath) {
+char *brainfuck_read_file(const char *path) {
     // Open the file from the path in read mode and handle the error for it.
-    FILE *file = fopen(filepath, "r");
+    FILE *file = NULL;
+    file = fopen(path, "r");
     if (NULL == file) {
-        fprintf(stderr, "Unable to open file `%s`.\n", filepath);
+        brainfuck_err_init("Unable to open file `%s`\n", path);
         return NULL;
-    } /* if the file is not open and is NULL */
+    }
 
+    // Move the file cursor to the end of the file and get its position.
     fseek(file, 0, SEEK_END);
-    int source_len = ftell(file);
-    char *source = (char *)malloc(source_len + 1);
+    size_t source_len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *source = NULL;
+    source = (char *)malloc(source_len + 1);
     if (NULL == source) {
-        fprintf(stderr, "Failed to allocate memory for the source.\n");
+        brainfuck_err_init("Failed to allocate memory space.\n");
         fclose(file);
         return NULL;
-    } /* if failed to allocate memory for the source */
-    rewind(file);
+    }
 
-    for (int i = 0; i < source_len; ++i) source[i] = getc(file);
-    source[source_len] = '\0';
+    // Read file content to the buffer.
+    fread(source, 1, source_len, file);
+    source[source_len] = 0;
 
     fclose(file);
     return source;
 }
 
-void errprintf(int ch, const char *fmt, ...) {
+void brainfuck_err_init(const char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
 
-    fprintf(
-        stderr, "\nFile \"%s\", char %d\nError: ",
-        bf_compile_filepath, ch + 1
-    );
+    // Error during initialization:
+    // Unable to open file `hello.bf`
+    fprintf(stderr, "Error during initialization:\n");
+    vfprintf(stderr, fmt, ap);
+
+    va_end(ap);
+}
+
+void brainfuck_err_code(int ch, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    // File "hello.bf", char 4
+    // Error: Byte overflow (255)
+    fprintf(stderr, "\nFile \"%s\", char %d\nError: ", brainfuck_path_input, ch);
     vfprintf(stderr, fmt, ap);
 
     va_end(ap);
